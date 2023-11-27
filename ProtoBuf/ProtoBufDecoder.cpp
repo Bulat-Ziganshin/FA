972bdc3f7e2e9ed68dd65536dd6bbc41c1e24b09
Bulat Ziganshin <bulat.ziganshin@gmail.com>
2023-11-27 05:26:04 +0300

    ProtoBuf: reordered code

--- ProtoBuf/ProtoBufDecoder.cpp
+++ ProtoBuf/ProtoBufDecoder.cpp
@@ -16,6 +16,77 @@ struct ProtoBufDecoder
     }
 
 
+    template <typename FixedType>
+    static FixedType read_fixed_width(void* ptr)
+    {
+        // TODO: reverse byte order on big-endian computers
+        FixedType value;
+        memcpy(&value, ptr, sizeof(value));
+        return value;
+    }
+
+    uint64_t read_varint()
+    {
+        uint64_t value = 0;
+        int shift = 0;
+
+        do {
+            if(ptr == buf_end)  throw std::runtime_error("Unexpected end of buffer in varint");
+            if(shift >= 64)     throw std::runtime_error("More than 10 bytes in varint");
+
+            auto has_more_bytes = (*ptr & 128);
+            uint64_t byte = (*ptr & 127);
+
+            value |= (byte << shift);
+            ptr++;  shift += 7;
+        } while(has_more_bytes);
+
+        return value;
+    }
+
+
+    uint64_t parse_integer_value(int field_type)
+    {
+        if (field_type == FT_VARINT) {
+            return read_varint();
+        }
+
+        if (field_type == FT_FIXED64) {
+            advance_ptr(8);
+            return read_fixed_width<uint64_t>(ptr-8);
+        }
+
+        if (field_type == FT_FIXED32) {
+            advance_ptr(4);
+            return read_fixed_width<uint32_t>(ptr-4);
+        }
+
+        throw std::runtime_error("Can't parse integral value with field type " + std::to_string(field_type));
+    }
+
+    int64_t parse_zigzag_integer_value(int field_type)
+    {
+        if(field_type != FT_VARINT) {
+            throw std::runtime_error("Can't parse zigzag integer with field type " + std::to_string(field_type));
+        }
+
+        uint64_t value = read_varint();
+        return int64_t((value>>1) | (value<<63));
+    }
+
+    std::string_view parse_bytearray_value(int field_type)
+    {
+        if(field_type != FT_LEN) {
+            throw std::runtime_error("Can't parse bytearray with field type " + std::to_string(field_type));
+        }
+
+        uint64_t len = read_varint();
+        advance_ptr(len);
+
+        return {ptr-len, len};
+    }
+
+
     bool get_next_field(int* field_num, int* field_type)
     {
         if(ptr == buf_end)  return false;
@@ -85,76 +156,5 @@ struct ProtoBufDecoder
     void parse_repeated_bytearray_field(int field_type, RepeatedByteArrayType *field)
     {
         field->push_back( parse_bytearray_value(field_type));
-    }
-
-
-    std::string_view parse_bytearray_value(int field_type)
-    {
-        if(field_type != FT_LEN) {
-            throw std::runtime_error("Can't parse bytearray with field type " + std::to_string(field_type));
-        }
-
-        uint64_t len = read_varint();
-        advance_ptr(len);
-
-        return {ptr-len, len};
-    }
-
-    int64_t parse_zigzag_integer_value(int field_type)
-    {
-        if(field_type != FT_VARINT) {
-            throw std::runtime_error("Can't parse zigzag integer with field type " + std::to_string(field_type));
-        }
-
-        uint64_t value = read_varint();
-        return int64_t((value>>1) | (value<<63));
-    }
-
-    uint64_t parse_integer_value(int field_type)
-    {
-        if (field_type == FT_VARINT) {
-            return read_varint();
-        }
-
-        if (field_type == FT_FIXED64) {
-            advance_ptr(8);
-            return read_fixed_width<uint64_t>(ptr-8);
-        }
-
-        if (field_type == FT_FIXED32) {
-            advance_ptr(4);
-            return read_fixed_width<uint32_t>(ptr-4);
-        }
-
-        throw std::runtime_error("Can't parse integral value with field type " + std::to_string(field_type));
-    }
-
-
-    uint64_t read_varint()
-    {
-        uint64_t value = 0;
-        int shift = 0;
-
-        do {
-            if(ptr == buf_end)  throw std::runtime_error("Unexpected end of buffer in varint");
-            if(shift >= 64)     throw std::runtime_error("More than 10 bytes in varint");
-
-            auto has_more_bytes = (*ptr & 128);
-            uint64_t byte = (*ptr & 127);
-
-            value |= (byte << shift);
-            ptr++;  shift += 7;
-        } while(has_more_bytes);
-
-        return value;
-    }
-
-    template <typename FixedType>
-    static FixedType read_fixed_width(void* ptr)
-    {
-        // TODO: reverse byte order on big-endians
-        FixedType value;
-        memcpy(&value, ptr, sizeof(value));
-        return value;
     }
 }
