56ef5a6e1204b6d0d05901e91c5f70e26589309a
Bulat Ziganshin <bulat.ziganshin@gmail.com>
2023-11-28 04:22:42 +0300

    ProtoBuf: added all supported field types to Example

--- ProtoBuf/Example.pb.cpp
+++ ProtoBuf/Example.pb.cpp
@@ -14,14 +14,21 @@ struct SubMessage
 
 struct Filter
 {
-    int64_t size = 42;
-    std::string_view name = "DEFAULT NAME";
-    std::vector<int64_t> values;
-    std::vector<std::string_view> subnames;
-    SubMessage msg;
-    std::vector<SubMessage> more_msgs;
+    int64_t     size;
+    int32_t     altitude;
+    float       weight;
+    std::string name = "DEFAULT NAME";
+    SubMessage  msg;
+
+    std::vector<uint32_t>    more_ints;
+    std::vector<int64_t>     more_sints;
+    std::vector<double>      more_floats;
+    std::vector<std::string> more_strings;
+    std::vector<SubMessage>  more_msgs;
 
     bool has_size = false;
+    bool has_altitude = false;
+    bool has_weight = false;
     bool has_name = false;
     bool has_msg = false;
 
@@ -37,13 +44,23 @@ void Filter::ProtoBufDecode(ProtoBufDecoder &pb)
     {
         switch(field_num)
         {
-            case 1: pb.parse_integral_field( field_type, &size, &has_size); break;
-            case 2: pb.parse_bytearray_field( field_type, &name, &has_name); break;
-            case 3: pb.parse_repeated_integral_field( field_type, &values); break;
-            case 4: pb.parse_repeated_bytearray_field( field_type, &subnames); break;
-            case 5: pb.parse_message_field( field_type, &msg, &has_msg); break;
-            case 6: pb.parse_repeated_message_field( field_type, &more_msgs); break;
+            case 1: pb.parse_integral_field        (field_type, &size,     &has_size); break;
+            case 2: pb.parse_zigzag_integral_field (field_type, &altitude, &has_altitude); break;
+            case 3: pb.parse_fp_field              (field_type, &weight,   &has_weight); break;
+            case 4: pb.parse_bytearray_field       (field_type, &name,     &has_name); break;
+            case 5: pb.parse_message_field         (field_type, &msg,      &has_msg); break;
+
+            case 11: pb.parse_repeated_integral_field       ( field_type, &more_ints); break;
+            case 12: pb.parse_repeated_zigzag_integral_field( field_type, &more_sints); break;
+            case 13: pb.parse_repeated_fp_field             ( field_type, &more_floats); break;
+            case 14: pb.parse_repeated_bytearray_field      ( field_type, &more_strings); break;
+            case 15: pb.parse_repeated_message_field        ( field_type, &more_msgs); break;
+
             default: pb.skip_field( field_type);
         }
+    }
+
+    if(! has_size) {
+        throw std::runtime_error("Decoded protobuf has no required field Filter.size");
     }
 }
