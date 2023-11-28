10f6f630cba18a01f2f1f0a901f30ccd08a9c098
Bulat Ziganshin <bulat.ziganshin@gmail.com>
2023-11-29 01:18:46 +0300

    ProtoBuf: generate decoding routines from .pbs

--- ProtoBuf/src/generator/generator.cpp
+++ ProtoBuf/src/generator/generator.cpp
@@ -21,14 +21,57 @@ constexpr const char* FILE_TEMPLATE = R"---(// This file was auto-generated from
 )---";
 
 constexpr const char* MESSAGE_TEMPLATE = R"---(
-struct {}
+struct {0}
 {{
-{}
-{}
+{1}
+{2}
     void ProtoBufDecode(ProtoBufDecoder &pb);
 }};
+
+
+void {0}::ProtoBufDecode(ProtoBufDecoder &pb)
+{{
+    int field_num, wire_type;
+
+    while( pb.get_next_field( &field_num, &wire_type))
+    {{
+        switch(field_num)
+        {{
+{3}
+            default: pb.skip_field(wire_type);
+        }}
+    }}
+}}
 )---";
 
+
+
+std::string_view domain_string(FieldDescriptorProto &field)
+{
+    switch(field.type)
+    {
+        case FieldDescriptorProto::TYPE_DOUBLE:
+        case FieldDescriptorProto::TYPE_FLOAT:
+            return "fp";
+
+        case FieldDescriptorProto::TYPE_SINT32:
+        case FieldDescriptorProto::TYPE_SINT64:
+            return "zigzag";
+
+        case FieldDescriptorProto::TYPE_STRING:
+        case FieldDescriptorProto::TYPE_BYTES:
+            return "bytearray";
+
+        case FieldDescriptorProto::TYPE_MESSAGE:
+            return "message";
+
+        case FieldDescriptorProto::TYPE_GROUP:
+            return "?group";
+
+        default:
+            return "integral";
+    }
+}
 
 std::string_view scalar_type_string(FieldDescriptorProto &field)
 {
@@ -74,11 +117,12 @@ void generator(FileDescriptorSet &proto)
 
     for (auto message_type: file.message_type)
     {
-        std::string fields_defs, has_fields_defs;
+        std::string fields_defs, has_fields_defs, decode_cases;
 
         for (auto field: message_type.field)
         {
-            std::string default_str = "";
+            // Generate message structure
+            std::string default_str;
             if (field.has_default_value) {
                 bool is_bytearray_field = (field.type==FieldDescriptorProto::TYPE_STRING || field.type==FieldDescriptorProto::TYPE_BYTES);
                 const char* quote_str = (is_bytearray_field? "\"" : "");
@@ -91,9 +135,21 @@ void generator(FileDescriptorSet &proto)
             if (field.label != FieldDescriptorProto::LABEL_REPEATED) {
                 has_fields_defs += std::format("    bool has_{} = false;\n", field.name);
             }
+
+            // Generate message decoding function
+            auto domain = domain_string(field);
+            std::string decoder;
+
+            if (field.label == FieldDescriptorProto::LABEL_REPEATED) {
+                decoder = std::format("pb.parse_repeated_{}_field( wire_type, &{})", domain, field.name);
+            } else {
+                decoder = std::format("pb.parse_{0}_field( wire_type, &{1}, &has_{1})", domain, field.name);
+            }
+
+            decode_cases += std::format("            case {}: {}; break;\n", field.number, decoder);
         }
 
-        std::cout << std::format(MESSAGE_TEMPLATE, message_type.name, fields_defs, has_fields_defs);
+        std::cout << std::format(MESSAGE_TEMPLATE, message_type.name, fields_defs, has_fields_defs, decode_cases);
     }
 }
 
